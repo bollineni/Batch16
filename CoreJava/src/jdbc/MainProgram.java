97fb4f79f7b9fc10b2813bc2815e571148ec4f82
Jayram <jayram.h2kinfosys@gmail.com>
2015-02-19 21:07:59 -0500

    Udpate

---
+++ CoreJava/src/jdbc/MainProgram.java
@@ -0,0 +1,33 @@
+package jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Emp{
+	String name;
+	String place;
+}
+public class MainProgram {
+
+	public static void main(String[] args) {
+		List<Emp> employees = null; 
+		employees = getAllEmployees();
+		for(Emp emp : employees){
+			
+		}
+	}
+	
+	public List<Emp> getAllEmployees() {
+		// List <Emp> emps = new ArrayList<Emp>();
+		// select query
+		// result set
+		// Emp emp = null;
+		// while(rs.next){
+		//  emp = new Emp();
+		// rs.setName(rs.getString("name"));
+		// emps.add(emp);
+		//}
+		// return emps;
+		return null;
+	}
+}
