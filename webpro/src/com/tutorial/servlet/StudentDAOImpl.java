3dbbe7610e59b05ae146efbb02d8a4fd27bc302e
Jayram <jayram.h2kinfosys@gmail.com>
2015-02-22 21:59:10 -0500

    Update

--- webpro/src/com/tutorial/servlet/StudentDAOImpl.java
+++ webpro/src/com/tutorial/servlet/StudentDAOImpl.java
@@ -1,8 +1,30 @@
 package com.tutorial.servlet;
 
+import java.sql.SQLException;
+
 public class StudentDAOImpl {
 	// All the db connection
 	// getEmployeeInfo(String fn , String ln){
 	
  //}
+	
+	/*public List selectQuery(int salary) {
+		Employee emp = null;
+		List list = new ArrayList();
+		try{
+			stmt = con.createStatement(); // Step 3
+			rs = stmt.executeQuery("select * from employees where salary > "+empid); // Step 4
+			while(rs.next()){
+				emp = new Employee();
+				emp.setName(rs.getString("FirstName"));
+				emp.setDesignation(rs.getString("Designation"));
+				list.add(emp);
+			}
+		}catch(SQLException exp){
+			exp.printStackTrace();
+		}finally{
+			closeConnections(rs,stmt,con);
+		}
+		return list;
+	}*/
 }
